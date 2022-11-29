0a11b252e007d0de9e99778fc986bf6c27aebb68
Eben-Success <aebenezer237@gmail.com>
2022-11-29 23:40:27 +0000

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import reactjs   from '../images/reactjs-icon.png';
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav>
+            <img className='nav--icon' src={reactjs} alt="" />
+            <h3 className="nav--logo_text" >ReactFacts</h3>
+            <h4 className='nav--title' >React Course - Project 1</h4>
+        </nav>
     )
 }
 
