1173a4292343514a124c4da586e941282239719b
Eben-Success <aebenezer237@gmail.com>
2022-11-30 22:19:36 +0000

    update

--- airbnb/src/component/Navbar.js
+++ airbnb/src/component/Navbar.js
@@ -3,7 +3,7 @@ import reactjs   from '../images/reactjs-icon.png';
 const Navbar = () => {
     return (
         <nav>
-            <img className='nav--icon' src={reactjs} alt="" />
+            <img className='nav--icon' src={Vector.png} alt="airbnb_logo" />
             <h3 className="nav--logo_text" >ReactFacts</h3>
             <h4 className='nav--title' >React Course - Project 1</h4>
         </nav>
