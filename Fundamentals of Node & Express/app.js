adae8ed54b8b8908393d214376e9cac6dd5245ed
Eben-Success <aebenezer237@gmail.com>
2022-12-04 22:55:28 +0000

    update

--- Fundamentals of Node & Express/app.js
+++ Fundamentals of Node & Express/app.js
@@ -1,7 +1,9 @@
 const names = require('./names')
 const sayHi = require('./util')
 const {eben, john} = require("./names");
+const data = require('./alternative-flavor')
 
+console.log(data)
 console.log(names)
 console.log(sayHi(john))
 console.log(sayHi(eben))
