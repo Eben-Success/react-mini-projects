d1f5d50ba3687046a1860f5319040fbdb15183e0
Eben-Success <aebenezer237@gmail.com>
2022-12-05 08:44:42 +0000

    update

---
+++ Fundamentals of Node & Express/5-util.js
@@ -0,0 +1,10 @@
+// Modules
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name} `);
+}
+
+// export default
+module.exports = sayHi
