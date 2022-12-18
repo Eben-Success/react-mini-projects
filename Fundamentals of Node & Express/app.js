ddaced6976020c6bf732a30800a27cc05e3ae697
Eben-Success <aebenezer237@gmail.com>
2022-12-18 04:03:04 +0000

    update

--- Fundamentals of Node & Express/app.js
+++ Fundamentals of Node & Express/app.js
@@ -1,4 +1,7 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 const getText = (path) => {
     return new Promise((resolve, reject)=>{
@@ -12,6 +15,23 @@ const getText = (path) => {
     })
 }
 
-getText(('./content/first.txt'))
-    .then(result => console.log(result))
-    .catch((err) => console.log(err))
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-grenade.txt',
+            `THIS IS AWESOME:
+             ${first} ${second}`, {flag: 'a'})
+        console.log(first, second);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
